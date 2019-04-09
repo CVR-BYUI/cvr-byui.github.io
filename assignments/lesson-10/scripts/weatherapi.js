32a0dcbc815bf40467742afc70ad180805d5fbdf
CVR-BYUI <cvanry@byui.edu>
2019-04-09 01:25:10 -0600

    w10

---
+++ assignments/lesson-10/scripts/weatherapi.js
@@ -0,0 +1,15 @@
+let weatherRequest = new XMLHttpRequest();
+
+weatherRequest.open('GET', 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=472837bd2aa3046b5d16b832ef0dd806', true);
+
+weatherRequest.send();
+
+weatherRequest.onload = function () {
+
+    let weatherData = JSON.parse(weatherRequest.responseText);
+
+    console.log(weatherData);
+
+    document.getElementById('current-temp').innerHTML = weatherData.list[0].main.temp;
+    
+}
