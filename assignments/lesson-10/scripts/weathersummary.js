d32d929d7917e93ca26e32ede5b6deb16a66d74a
CVR-BYUI <cvanry@byui.edu>
2019-04-09 03:51:43 -0600

    update

---
+++ assignments/lesson-10/scripts/weathersummary.js
@@ -0,0 +1,19 @@
+var weatherRequest = new XMLHttpRequest
+weatherRequest.open('GET', 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=472837bd2aa3046b5d16b832ef0dd806&units=imperial', true);
+weatherRequest.send();
+weatherRequest.onload = function () {
+
+    var weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+
+    // description, temp, humidity, windspeed
+    document.getElementById('weatherDesc').innerHTML = weatherData.weather[0].description;
+    document.getElementById('highTemp').innerHTML = weatherData.main.temp_max;
+    document.getElementById('humidity').innerHTML = weatherData.main.humidity;
+    document.getElementById('windSpeed').innerHTML = weatherData.wind.speed;
+
+    // wind chill
+    var windChill = 35.74 + 0.6215 * weatherData.main.temp - 35.75 * Math.pow(weatherData.wind.speed, 0.16) + 0.4275 * weatherData.main.temp * Math.pow(weatherData.wind.speed, 0.16);
+    windChill = Math.round(windChill);
+    document.getElementById("windChill").innerHTML = windChill;
+}
