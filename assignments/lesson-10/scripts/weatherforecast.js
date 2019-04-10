bdb5595826c041860c53836cf8171c9e32481077
CVR-BYUI <cvanry@byui.edu>
2019-04-10 01:59:46 -0600

    forecast

--- assignments/lesson-10/scripts/weatherforecast.js
+++ assignments/lesson-10/scripts/weatherforecast.js
@@ -1,65 +1,57 @@
-let section = document.querySelector('section');
 let weatherForecast = new XMLHttpRequest
 weatherForecast.open('GET', 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=472837bd2aa3046b5d16b832ef0dd806&units=imperial', true);
 weatherForecast.send();
 weatherForecast.onload = function () {
 
-    let forecast = JSON.parse(weatherForecast.responseText);
+    var weatherInfo = JSON.parse(weatherForecast.responseText);
+    console.log(weatherInfo);
 
-    console.log(forecast);
+    var listWeekday = [];
+    var listTemp = [];
+    var listIconCode = [];
 
-    let dataLoop = forecast['list'];
+    var weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     
-    for (let i = 0; i < dataLoop.length; i++) {
-        
-            if (dataLoop[i].dt_txt.includes('18:00:00')) {
+    
+    for (i = 0; i < weatherInfo.list.length; ++i) {
+        time = weatherInfo.list[i].dt_txt;
+        if (time.includes("18:00:00")) {
 
-                var divSection = document.createElement('div');
-                var forecastDay = document.createElement('div');
-                var imageIcon = document.createElement('img');
-                var imageLink = 'http://openweathermap.org/img/w/' + dataLoop[i].weather[0].icon + '.png';
-                var tempForecast = document.createElement('div');
+            // Weekday
+            var findDate = weekday[date.getDay()];
+            listWeekday.push(findDate);
 
-                divSection.setAttribute('class', 'fivedays');
+            // Current Temp
+            var temp = weatherInfo.list[i].main.temp;
+            var temp = Math.round(temp);
+            listTemp.push(temp);
 
-                var n = new Date(dataLoop[i].dt_txt);
-                var day = n.getDay();
-
-                switch (day) {
-                    case 0:
-                        day = "Sun";
-                        break;
-                    case 1:
-                        day = "Mon";
-                        break;
-                    case 2:
-                        day = "Tue";
-                        break;
-                    case 3:
-                        day = "Wed";
-                        break;
-                    case 4:
-                        day = "Thur";
-                        break;
-                    case 5:
-                        day = "Fri";
-                        break;
-                    case  6:
-                        day = "Sat"; 
-                }
-                      
-                forecastDay.textContent = day;
-                imageIcon.setAttribute('src', imageLink);
-                tempForecast.textContent = dataLoop[0].main.temp + "&deg; F";
-
-                divSection.appendChild(forecastDay);
-                divSection.appendChild(imageIcon);
-                divSection.appendChild(tempForecast);
-
-                section.appendChild(divSection);
-                
-            }
-     
+            // Weather icon
+            var iconcode = weatherInfo.list[i].weather["0"].icon;
+            var icon_path = "https://openweathermap.org/img/w/" + iconcode + ".png";
+            listIconCode.push(icon_path);
+        }
     }
+
+    //Display forecast day of the week
+    document.getElementById('day1').innerHTML = listWeekday[0];
+    document.getElementById('day2').innerHTML = listWeekday[1];
+    document.getElementById('day3').innerHTML = listWeekday[2];
+    document.getElementById('day4').innerHTML = listWeekday[3];
+    document.getElementById('day5').innerHTML = listWeekday[4];
+
+    //Display weather icon
+    document.getElementById('weather_icon1').src = listIconCode[0];
+    document.getElementById('weather_icon2').src = listIconCode[1];
+    document.getElementById('weather_icon3').src = listIconCode[2];
+    document.getElementById('weather_icon4').src = listIconCode[3];
+    document.getElementById('weather_icon5').src = listIconCode[4];
+
+    //Display forecasted temperature
+    document.getElementById("temp1").innerHTML = listTemp[0];
+    document.getElementById("temp2").innerHTML = listTemp[1];
+    document.getElementById("temp3").innerHTML = listTemp[2];
+    document.getElementById("temp4").innerHTML = listTemp[3];
+    document.getElementById("temp5").innerHTML = listTemp[4];
 
 }
