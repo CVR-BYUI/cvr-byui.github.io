e0326b04174ae5b1d8d5cb61810ceca78d53fa8c
CVR-BYUI <cvanry@byui.edu>
2019-04-10 03:52:38 -0600

    update

---
+++ assignments/lesson-10/scripts/townevents.js
@@ -0,0 +1,31 @@
+let request = new XMLHttpRequest();
+request.open('GET', 'https://byui-cit230.github.io/weather/data/towndata.json');
+request.responseType = 'json';
+request.send();
+
+request.onload = function () {
+
+    let town = request.response;
+    showTownEvent(town);
+
+}
+
+function showTownEvent(jsonObj) {
+
+    let data = jsonObj['towns'];
+    let listEvent = [];
+
+    for (var i = 0; i < data.length; i++) {
+        var name = data[i].name;
+        if (name.includes("Preston")) {
+
+        let event = data.list[i].events;
+        listEvent.push(event);
+        }
+    }
+
+    document.getElementById('event1').innerHTML = listEvent[0];
+    document.getElementById('event2').innerHTML = listEvent[1];
+    document.getElementById('event3').innerHTML = listEvent[2];
+
+}
