15892cda347ee88852216bec900a7d62fcceefd8
CVR-BYUI <cvanry@byui.edu>
2019-04-12 22:45:42 -0600

    final project

---
+++ assignments/finalproject/scripts/templeinfo-provocitycenter.js
@@ -0,0 +1,58 @@
+let request = new XMLHttpRequest();
+request.open('GET', 'https://raw.githubusercontent.com/CVR-BYUI/cvr-byui.github.io/master/assignments/finalproject/json/templedata.json');
+request.responseType = 'json';
+request.send();
+
+request.onload = function () {
+
+    let templeData = request.response;
+    showTempleData(templeData);
+
+}
+
+    function showTempleData(jsonObj) {
+
+    let templeData = jsonObj['templeData'];
+
+    for (let i = 0; i < templeData.length; i++) {
+        let temple = templeData[i].city;
+
+        if (temple.includes("Provo")) {
+
+            document.getElementById('templeName').innerHTML = templeData[i].templeName;
+            document.getElementById('address1').innerHTML = templeData[i].address1;
+            document.getElementById('address2').innerHTML = templeData[i].address2;
+            document.getElementById('address3').innerHTML = templeData[i].address3;
+            document.getElementById('telephone').innerHTML = templeData[i].telephone;
+            document.getElementById('email').innerHTML = templeData[i].email;
+            document.getElementById('announced').innerHTML = templeData[i].announced;
+            document.getElementById('groundbreaking').innerHTML = templeData[i].groundbreaking;
+            document.getElementById('dedicated').innerHTML = templeData[i].dedicated;
+
+            // services with line breaks
+            let services = templeData[i].services;
+            let servicesList = "";
+            for (let p = 0; p < services.length; p++) {
+                servicesList += services[p] + "<br>";
+            }
+            document.getElementById('services').innerHTML = servicesList;
+
+            // schedule with line breaks
+            let schedule = templeData[i].schedule;
+            let scheduleList = "";
+            for (let p = 0; p < schedule.length; p++) {
+                scheduleList += schedule[p] + "<br>";
+            }
+            document.getElementById('schedule').innerHTML = scheduleList;
+
+            // closures with line breaks
+            let closures = templeData[i].closures;
+            let closuresList = "";
+            for (let p = 0; p < closures.length; p++) {
+                closuresList += closures[p] + "<br>";
+            }
+            document.getElementById('closures').innerHTML = closuresList;
+
+        }
+    }
+}
---
+++ assignments/finalproject/scripts/templeweather-atlanta.js
@@ -0,0 +1,29 @@
+let weatherRequest = new XMLHttpRequest
+weatherRequest.open('GET', 'https://api.openweathermap.org/data/2.5/forecast?id=4180439&appid=472837bd2aa3046b5d16b832ef0dd806&units=imperial', true);
+weatherRequest.send();
+weatherRequest.onload = function () {
+
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+
+    // description, temp, humidity, windspeed
+    document.getElementById('weatherDesc').innerHTML = weatherData.list[0].weather[0].description;
+    document.getElementById('currentTemp').innerHTML = weatherData.list[0].main.temp;
+    document.getElementById('humidity').innerHTML = weatherData.list[0].main.humidity;
+    document.getElementById('windSpeed').innerHTML = weatherData.list[0].wind.speed;
+
+    // wind chill
+    let t = document.getElementById('currentTemp').innerHTML = weatherData.list[0].main.temp;
+    let x = document.getElementById('windSpeed').innerHTML = weatherData.list[0].wind.speed;
+    let s = Math.pow(x, 0.16);
+    let windChill = 35.74 + (0.6215 * t) - (35.75 * s) + (0.4275 * t * s);
+    windChill = Math.round(windChill);
+    document.getElementById('windChill').innerHTML = windChill;
+
+    // wind direction
+    let degree = weatherData.list[0].wind.deg;
+    let compass = Math.round((degree - 11.25) / 22.5);
+    let windNames = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+    let direction = windNames[compass];
+    document.getElementById('windDirection').innerHTML = direction;
+}
